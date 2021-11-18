cafb8937c7b688a8a70398620a55a032901570ba
dudu340 <60038244+dudu340@users.noreply.github.com>
2021-11-18 18:00:26 +0800

    Add files via upload

---
+++ Tabs.vue
@@ -0,0 +1,51 @@
+<template>
+	<view>
+		<view class="content_items">
+			<view class="content_item" 
+				v-for="(item,index) in tabs" 
+				:key="item.index" 
+				:class="item.isActive ? 'active' : ''"
+				@tap="handlerActive(item.index)"
+			>
+				{{item.value}}
+			</view>
+		</view>
+		<slot></slot>
+	</view>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		},
+		props: {
+			tabs: {
+				type: Array,
+				default: []
+			}
+		},
+		methods:{
+			handlerActive(index){
+				this.$emit('tabActive',index)
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+.content_items {
+		display: flex;
+		.content_item {
+			text-align: center;
+			float: left;
+			padding: 20px,25px;
+			flex: 1;
+		}
+	}
+	.active {
+		color: #EB4450;
+		border-bottom: 5rpx solid #DD524D;
+	}
+</style>
---
+++ tabs_detail.vue
@@ -0,0 +1,85 @@
+<template>
+	<view>
+		<view class="content">
+			<tabs :tabs='tablist' v-on:tabActive='tabActive' />
+				<block v-if="contentIndex==0">
+					<view class="swiperr">
+						<swiper :indicator-dots="true" :autoplay="true" :interval="2000" :duration="1000" circular="true">
+							<swiper-item>
+								<view class="swiper-item" @tap="previewpic(0)">
+									<image class="images" src="../../static/3.png"></image>
+								</view>
+							</swiper-item>
+							<swiper-item>
+								<view class="swiper-item" @tap="previewpic(1)">
+									
+								</view>
+							</swiper-item>
+						</swiper>
+					</view>	
+				</block>
+				<block v-if="contentIndex==1">
+					第二页内容
+					<button  size="middle">添加特别关注院校</button  >
+				</block>
+				<block v-if="contentIndex==2">
+					第三页内容
+				</block>
+			</tabs>
+		</view>
+	</view>
+</template>
+<script>
+	import tabs from '../../components/Tabs.vue'
+	export default {
+		data() {
+			return {
+				tablist: [{
+						index: 0,
+						value: '已发布',
+						isActive: true
+					},
+					{
+						index: 1,
+						value: '特别关注',
+						isActive: false
+					},
+					{
+						index: 2,
+						value: '已删除',
+						isActive: false
+					}
+				],
+				contentIndex: '',
+				images: []
+			}
+		},
+		methods: {
+			tabActive(tabIndex) {
+				this.tablist.map((value, index) => {
+					value.isActive = tabIndex == index ? true : false
+				})
+				this.contentIndex = tabIndex
+			},
+		//轮播图实现图片预览功能
+			previewpic(itemsrc){
+				uni.previewImage({
+					current:itemsrc,
+					urls:["../../static/3.png","../../static/4.png"]
+				})
+			}
+		},
+		components: {
+			tabs,
+		}
+	}
+</script>
+<style lang="scss">
+	swiper {
+		height: 400rpx;
+		image {
+			width: 100%;
+			height: 100vh;
+		}
+	}
+</style>
